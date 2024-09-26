ca570837cffd27c9422f23314015abfcdeb771fc
ririsahu <ririsahu1600@gmail.com>
2024-09-26 10:39:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+const App = () => {
+    const [time, settime ] = useState(new Date().toLocaleTimeString());
+    setInterval(() => {
+        settime(new Date().toLocaleTimeString());
+    },1000);
+  return (
+    <div>
+        <h1> {time}</h1>
+    </div>
+  )
+};
+
+export default App;
